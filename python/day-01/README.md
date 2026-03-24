8fe57d0ddfb7544991409d7fd7d86c4c74e6dfc2
Pawel12master <pawelorzel1999@gmail.com>
2026-03-24 22:44:31 +0100

    day 01 - port scanner using socet module

--- python/day-01/README.md
+++ python/day-01/README.md
@@ -1,2 +1,4 @@
 # Port Scanner
 Prosty skaner portów TCP
+
+Jak użyć: python port_scanner.py <adres> <port>
--- python/day-01/port_scanner.py
+++ python/day-01/port_scanner.py
@@ -1,1 +1,22 @@
-print("Hello")
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print(f"Użycie: python {sys.argv[0]} <adres> <max_port>")
+    sys.exit(1)
+
+
+adres = sys.argv[1]
+max_port = int(sys.argv[2])
+
+
+
+def check_ports(max_port,adres):
+    for x in range(1,max_port+1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)
+            wynik = s.connect_ex((adres, x))
+            if wynik == 0:
+                print(f"Dla portu: {x} status to: otwarty")
+
+check_ports(max_port,adres)
