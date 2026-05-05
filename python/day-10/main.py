82e616e7dfdbc07438213b44ff99f7bb8935b8d9
Pawel12master <pawelorzel1999@gmail.com>
2026-05-05 20:12:01 +0200

    simple hash cracker

--- python/day-10/main.py
+++ python/day-10/main.py
@@ -2,48 +2,59 @@ import hashlib
 import sys
 import re
 
-DICT = [
-    "jablko", "arbuz", "poduszka", "listwa", "rak", "wyspa"
-]
+DICT = [ "jablko", "arbuz", "poduszka", "listwa", "rak", "wyspa"]
 
-pattern_hash = re.compile(r"^[0-9a-f]+$", re.IGNORECASE)
+
 
 def check_hash(input_hash):
-    result = pattern_hash.match(input_hash)
-    if result is not None:
+    pattern_hash = re.compile(r"^[0-9a-f]+$", re.IGNORECASE)
+    if pattern_hash.match(input_hash):
         return True
     else:
         return False
     
 def check_length(input_hash):
-    hash_type = ""
-    if len(input_hash) == 32:
-        hash_type = "md5"
-    elif len(input_hash) == 40:
-        hash_type = "sha-1"
-    elif len(input_hash) == 56:
-        hash_type = "sha-224"
-    elif len(input_hash) == 128:
-        hash_type = "sha-512"
+    dlugosc = len(input_hash)
+    if dlugosc == 32:
+        return "md5"
+    elif dlugosc == 40:
+        return "sha-1"
+    elif dlugosc == 56:
+        return "sha-224"
+    elif dlugosc == 128:
+        return "sha-512"
     else:
-        hash_type = "hash nieznany"
-    return hash_type
+        return "hash nieznany"
 
 if len(sys.argv) != 2:
     print("Podaj argument! Uzycie main.py <hash>")
     sys.exit(1)
+
 input_hash = sys.argv[1].lower()
 
-result = check_length(input_hash)
-for password in DICT:
-    match result:
-        case "md5":
+if check_hash(input_hash) == False:
+    print("To nie jest poprawny hash (powinien mieć tylko znaki 0-9 i a-f)")
+else:
+    type_hash = check_length(input_hash)
+    print(f"Rozpoznano typ {type_hash}")
+    if type_hash == "hash nieznany":
+        print("Nie znaleziono hasha")
+        sys.exit(1)
+    for password in DICT:
+        if type_hash == "md5":
             hash_obj = hashlib.md5(password.encode()).hexdigest()
-            if hash_obj == input_hash:
-                print(f"Haslo to:{password}")
-        case "hash nieznany":
-            print("Hash o nieznanej długości")
+        elif type_hash == "sha-1":
+            hash_obj = hashlib.sha1(password.encode()).hexdigest()
+        elif type_hash == "sha-224":
+            hash_obj = hashlib.sha224(password.encode()).hexdigest()
+        elif type_hash == "sha-512":
+            hash_obj = hashlib.sha512(password.encode()).hexdigest()
+        
+        if hash_obj == input_hash:
+            print(f"Znaleziono haslo:{password}")
             break
+        print("Nie znaleziono hasla w slowniku")
+        
 
 
 
